17bbe567eafc83db3eac11efca41e83f5bf19340
isaacgreenfield <ivtim708@gmail.com>
2025-11-10 12:43:43 +0300

    Finalized input

--- main.cpp
+++ main.cpp
@@ -1,25 +1,28 @@
 #include<iostream>
 
-struct IntArray {
+struct IntArray
+{
   void add(int i);
   int get(size_t id) const;
   size_t size() const;
 };
 
-int main() {
+int main()
+{
   int next = 0;
   std::cin >> next;
-  if (!std::cin)
-  {
-    return 1;
-  }
   IntArray a;
   a.add(next);
-  while (std::cin >> next) {
+  while (std::cin >> next)
+  {
     a.add(next);
   }
+  if (std::cin.fail()) {
+    return 1;
+  }
 
-  for (size_t i = 0; i < a.size(); ++i) {
+  for (size_t i = 0; i < a.size(); ++i)
+  {
     int d = a.get(i);
   }
 
