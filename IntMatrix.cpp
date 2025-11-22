a5d295b4771b5f4b1fbc44ab68ee24b36072004b
isaacgreenfield <ivtim708@gmail.com>
2025-11-22 22:43:27 +0300

    Created some extra functions and add_col

--- IntMatrix.cpp
+++ IntMatrix.cpp
@@ -1,47 +1,90 @@
 #include "IntMatrix.h"
-bool IntMatrix::add_col(size_t last_col, int x)
+#include <iostream>
+void IntMatrix::add_col(size_t last_col, int x)
+{
+  if (last_col == 0)
+  {
+    IntArray tmp = IntArray(x);
+    for (size_t i = 0; i < rows; ++i)
+    {
+      for (size_t j = 0; j < cols; ++j)
+      {
+        tmp.add(get(i * cols + j));
+      }
+      if (i != rows - 1) tmp.add(x);
+    }
+    data = tmp;
+  }
+  else
+  {
+    IntArray tmp = IntArray(get(0));
+    for (size_t i = 0; i < rows; ++i)
+    {
+      for (size_t j = 0; j < last_col; ++j)
+      {
+        tmp.add(get(i * cols + j));
+      }
+      tmp.add(x);
+      for (size_t j = last_col; j < cols; ++j)
+      {
+        tmp.add(get(i * cols + j));
+      }
+    }
+    data = tmp;
+  }
+  cols++;
+}
+void IntMatrix::add_row(size_t last_row, int x)
 {
 
 }
-bool IntMatrix::add_row(size_t last_row, int x)
-{
-
-}
-bool IntMatrix::expand(size_t last_row, size_t last_col)
+void IntMatrix::expand(size_t last_row, size_t last_col)
 {
 
 }
 
-void IntMatrix::fill(int i)
+void IntMatrix::fill(int x)
 {
-
+  IntArray tmp = IntArray(data.get(0));
+  for (size_t i = 0; i < size(); ++i)
+  {
+    tmp.add(data.get(i));
+  }
+  tmp.add(x);
+  data = tmp;
 }
 size_t IntMatrix::size() const noexcept
 {
-
+  return rows*cols;
 }
 int IntMatrix::get(size_t id) const noexcept
 {
-
+  return data.get(id);
+}
+void IntMatrix::show() const noexcept {
+  for (size_t i = 0; i < rows; ++i)
+  {
+    for (size_t j = 0; j < cols; ++j)
+    {
+      std::cout << get(i*cols + j) << " ";
+    }
+    std::cout << "\n";
+  }
 }
 
-IntMatrix::IntMatrix(size_t r, size_t c, int first_elem);
+IntMatrix::IntMatrix(size_t r, size_t c, int first_elem): rows(1), cols(1), data(IntArray(first_elem))
 {
   rows = r;
   cols = c;
-  data = new IntArray(first_elem);
 }
-IntMatrix::~IntMatrix() {
-  delete[] data;
-}
+IntMatrix::~IntMatrix() {}
 IntMatrix &IntMatrix::operator=(const IntMatrix &rhs) {
   IntArray tmp = IntArray(rhs.get(0));
   for (size_t i = 1; i < rhs.size(); ++i)
   {
     tmp.add(rhs.get(i));
   }
-  delete[] data;
-  data = &tmp;
+  data = tmp;
   rows = rhs.rows;
   cols = rhs.cols;
   return *this;
--- IntMatrix.h
+++ IntMatrix.h
@@ -1,19 +1,21 @@
 #ifndef INTMATRIX_H
 #define INTMATRIX_H
 #include "IntArray.h"
+#include <iostream>
 struct IntMatrix
 {
   size_t rows;
   size_t cols;
-  IntArray* data;
+  IntArray data;
 
-  void fill(int i);
+  void fill(int x);
   size_t size() const noexcept;
   int get(size_t id) const noexcept;
+  void show() const noexcept;
 
-  bool add_row(size_t last_row, int x);
-  bool add_col(size_t last_col, int x);
-  bool expand(size_t last_row, size_t last_col);
+  void add_row(size_t last_row, int x);
+  void add_col(size_t last_col, int x);
+  void expand(size_t last_row, size_t last_col);
 
   IntMatrix(size_t r, size_t c, int first_elem);
   ~IntMatrix();
